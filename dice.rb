9270672471878014333bda21d0a98b1012defa6e
waikyjung <waiky.jung@gmail.com>
2024-10-16 03:11:07 +0000

    templates

--- dice.rb
+++ dice.rb
@@ -1,6 +1,11 @@
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
 
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/zebra") do
   "We must add a route for each path we want to support"
@@ -11,14 +16,7 @@ get("/giraffe") do
 end
 
 get("/") do
-  "<h1>Dice Roll</h1>
-  <p></p>
-  <ul>
-    <li><a href=https://opulent-palm-tree-r4rjxjq6645q2pqvg-4567.app.github.dev/dice/2/6>Roll two 6-sided dice</a></li>
-    <li><a href=https://opulent-palm-tree-r4rjxjq6645q2pqvg-4567.app.github.dev/dice/2/10>Roll two 10-sided dice</a></li>
-    <li><a href=https://opulent-palm-tree-r4rjxjq6645q2pqvg-4567.app.github.dev/dice/1/20>Roll one 20-sided dice</a></li>
-    <li><a href=https://opulent-palm-tree-r4rjxjq6645q2pqvg-4567.app.github.dev/dice/5/4>Roll 5 4-sided dice</a></p>
-  </ul>"
+  erb(:elephant)
 end
 
 get("/dice/2/6") do
@@ -26,10 +24,9 @@ get("/dice/2/6") do
   second_die = rand(1..6)
   sum = first_die + second_die
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1>2d6</h1>
-    <p>#{outcome}</p>"
+  erb(:two_six)
 end
 
 get("/dice/2/10") do
@@ -37,20 +34,18 @@ get("/dice/2/10") do
   second_die = rand(1..10)
   sum = first_die + second_die
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}"
 
-  "<h1>2d10</h1>
-    <p>#{outcome}</p>"
+  erb(:two_ten)
 end
 
 get("/dice/1/20") do
   first_die = rand(1..10)
   sum = first_die
 
-  outcome = "You rolled a #{first_die} for a total of #{sum}"
+  @outcome = "You rolled a #{first_die} for a total of #{sum}"
 
-  "<h1>1d20</h1>
-    <p>#{outcome}</p>"
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
@@ -61,8 +56,7 @@ get("/dice/5/4") do
   fifth_die = rand(1..4)
   sum = first_die + second_die + third_die + fourth_die + fifth_die
 
-  outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and a #{fifth_die} for a total of #{sum}"
+  @outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and a #{fifth_die} for a total of #{sum}"
 
-  "<h1>5d4</h1>
-    <p>#{outcome}</p>"
+  erb(:five_four)
 end
